e6a569000a1bc302bd2369275d084fc983b710e2
SP-Gaara <54334044+SP-Gaara@users.noreply.github.com>
2019-11-26 21:19:45 +0200

    Update fortnite.js

--- Ep_12/commands/fun/fortnite.js
+++ Ep_12/commands/fun/fortnite.js
@@ -1,52 +1,54 @@
 const { RichEmbed } = require("discord.js");
 const { stripIndents } = require("common-tags");
 
-// Import the fortnite module
+
 const Client = require("fortnite");
 const ft = new Client(process.env.FORTNITE);
 
 module.exports = {
-    name: "fortnite",
+    name: "stats",
     aliases: ["ft"],
-    description: "Display someone's stats, the current store, and challenges!!",
+    description: "ÆSIR UP, Search for your fortnite stats",
     usage: "<username | store>",
     run: async (client, message, args) => {
-        // Available platforms
-        const platforms = ["pc", "xb1", "psn"];
         
-        // Here's the code for next video
+        
+        
+        const platforms = ["pc", "xbox", "psn"];
+        
+      
         if (args[0].toLowerCase() === "store") {
             
         } else {
-            // Get the last word from the args array
+           
             const lastWord = args[args.length - 1].toLowerCase();
             
-            // Declare variables so we can use them later
+            
             let platform, username; 
 
-            // If the platforms array includes the last word we provided
+           
             if (platforms.includes(lastWord)) {
-                // Make the username untill the last word
+                
                 username = args.slice(0, args.length - 1).join(" ");
-                // Make the platform the last word  
+                 
                 platform = lastWord;
             } else {    
-                // Make the username all words
+               
                 username = args.join(" ");
-                // Platform defaults to PC
+               
                 platform = "pc";
             }
             
-            // Search the user 
+            
             const search = await ft.user(username, platform);
 
-            // If the user isn't found
+            
             if (!search.username) {
                 return message.channel.send("Couldn't find that person, try again")
                     .then(m => m.delete(5000));
             }
 
-            // Declare stuffs for easier access
+           
             const lifetime = search.stats.lifetime;
             const solo = search.stats.solo;
             const duo = search.stats.duo;
